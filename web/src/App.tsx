57d8b1217ddd3faf1645ae180ea3002f65a7bf3c
Luís Henrique Machado <lhfam97@gmail.com>
2020-08-03 15:15:06 -0300

    First Commit

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./assets/styles/global.css";
+import Landing from "./pages/Landing";
+function App() {
+  return (
+    <div id="App">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
